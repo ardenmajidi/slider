057e115a83eff04298b754938449c9951bb9ab07
Arden <arden.majidi@gmail.com>
2022-03-10 16:20:27 +1100

    deploy slider

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+      </header>
+      <h1>Arden</h1>
+    </div>
+  );
+}
+
+export default App;
